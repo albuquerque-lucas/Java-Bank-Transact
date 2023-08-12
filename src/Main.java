bf935af02b10b699c6a7a14a5d21b99eb2e51857
albuquerque-lucas <lucaslpra@gmail.com>
2023-08-12 15:44:27 -0300

    Adicionando loop de operacoes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import controllers.OperationController;
 import controllers.UserController;
 import model.user.User;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         UserController userController = new UserController();
         userController.start();
         User mainUser = userController.initializeUser();
@@ -10,9 +14,16 @@ public class Main {
         userController.associateAccount(mainUser);
         User secondaryUser = userController.createSecondUser();
         OperationController operationController = new OperationController(mainUser, secondaryUser);
-        operationController.start();
-        String operation = operationController.chooseOperationType();
-        operationController.performOperation(operation);
+        boolean continueOperations = true;
+        while (continueOperations) {
+            operationController.start();
+            String operation = operationController.chooseOperationType();
+            operationController.performOperation(operation);
+
+            System.out.println("Deseja realizar outra operacao? (y/n)");
+            String response = scanner.nextLine();
+            continueOperations = response.equalsIgnoreCase("y");
+        }
     }
 
 
